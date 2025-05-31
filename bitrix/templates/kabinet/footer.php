aea7574dd9a52bb45196fd1a59519b4c7f49ce7c
sergey <sexiterra@mail.ru>
2025-05-31 21:17:39 +0300

    Исправлена ошибка с Pinia, использовать хранилище Pinia до вызова app.use(pinia)

--- bitrix/templates/kabinet/footer.php
+++ bitrix/templates/kabinet/footer.php
@@ -45,7 +45,8 @@ Dbg::showDebug();
 
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH."/assets/js/kabinet/vue-componets/extension/addnewmethods.js");
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH."/assets/js/kabinet/vue-componets/extension/helper.js");
-    
+
+    if (\PHelp::isAdmin()) Asset::getInstance()->addJs(SITE_TEMPLATE_PATH."/assets/js/kabinet/admin.application.js");
     //Asset::getInstance()->addJs(SITE_TEMPLATE_PATH."/assets/js/kabinet/test.js");
     ?>
 
