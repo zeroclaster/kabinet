def221c26a8144728561183925133e47ed364c1c
sergey <sexiterra@mail.ru>
2025-08-12 15:48:16 +0300

    Добавление к сылкам https://kupi-otziv.ru что бы в телеграме ссылки открывались.

--- bitrix/modules/kabinet/lib/messanger/config.php
+++ bitrix/modules/kabinet/lib/messanger/config.php
@@ -1,10 +1,10 @@
 <?
 $config = [
     'SYSTEM_MESSAGE' =>[
-    'postupil_otchet_na_proverku' => 'Поступил отчет на проверку – <a href="/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">открыть</a>',
-        'postupil_text_na_proverku' => 'Поступил текст на проверку – <a href="/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">открыть</a>',
-        'ispolnitel_ozhidaet_tekst' => 'Система ожидает от Вас текст – <a href="/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">открыть</a>',
-        'zadacha otmenena ispolnitelem' => 'Задача отменена администратором: <a href="/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">ссылка на исполнения задачи</a>',
+    'postupil_otchet_na_proverku' => 'Поступил отчет на проверку – <a href="https://kupi-otziv.ru/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">открыть</a>',
+        'postupil_text_na_proverku' => 'Поступил текст на проверку – <a href="https://kupi-otziv.ru/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">открыть</a>',
+        'ispolnitel_ozhidaet_tekst' => 'Система ожидает от Вас текст – <a href="https://kupi-otziv.ru/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">открыть</a>',
+        'zadacha otmenena ispolnitelem' => 'Задача отменена администратором: <a href="https://kupi-otziv.ru/kabinet/projects/reports/?t=#UF_TASK_ID#&id=#UF_QUEUE_ID#">ссылка на исполнения задачи</a>',
         ],
     'FILTER'=>[
         'FILTER_DEFAULT' => [],
