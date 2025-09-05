0fba1a79f41c2da861a7360c7912654a28e74084
sergey <sexiterra@mail.ru>
2025-09-05 10:00:30 +0300

    Исправления в выборке сообщений.

--- bitrix/modules/kabinet/lib/controller/messengerevents.php
+++ bitrix/modules/kabinet/lib/controller/messengerevents.php
@@ -119,6 +119,10 @@ class Messengerevents extends \Bitrix\Main\Engine\Controller
             $f['UF_PROJECT_ID'] = $post['UF_PROJECT_ID'];
         }
         elseif ($post['UF_TARGET_USER_ID'] != NULL && $post['UF_TARGET_USER_ID']>0){
+            $id_list = $post['UF_TARGET_USER_ID'];
+            $f = array_merge($f,[
+                'LOGIC' => 'AND',
+                ['LOGIC' => 'OR','UF_AUTHOR_ID'=>$id_list,'UF_TARGET_USER_ID'=>$id_list]]);
         }
         $messData = $messanger->getData(
             $f,
@@ -146,13 +150,23 @@ class Messengerevents extends \Bitrix\Main\Engine\Controller
             }
         }
 
+        if (!$f) {
+            $user = \Bitrix\Main\DI\ServiceLocator::getInstance()->get('user');
+            $user_id = $user['ID'];
+            $f = ['UF_STATUS'=>\Bitrix\Kabinet\messanger\Messanger::NEW_MASSAGE];
+            $f = array_merge(['LOGIC' => 'AND', ['LOGIC' => 'OR', 'UF_AUTHOR_ID' => $user_id, 'UF_TARGET_USER_ID' => $user_id]],$f);
+        }
+
         $data = LmessangerTable::getListActive([
             'select'=>[new \Bitrix\Main\Entity\ExpressionField('CNT', 'COUNT(*)')],
             'filter'=>$f
         ])->fetch();
 
+
         $returnData = ['count'=> $data['CNT']];
         $returnData['filter'] = $f;
+        $returnData['qeury'] = \Bitrix\Main\Entity\Query::getLastQuery();
+        $returnData['post'] = print_R($post,true);
         return $returnData;
     }
 }
