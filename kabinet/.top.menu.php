cb274f3198d1f55520ce0a0bc854b7ae75597f5e
sergey <sexiterra@mail.ru>
2025-10-26 20:50:35 +0300

    Новое меню слева у пользователся

--- kabinet/.top.menu.php
+++ kabinet/.top.menu.php
@@ -3,26 +3,5 @@ if (\PHelp::isAdmin()) {
     include $_SERVER["DOCUMENT_ROOT"].'/kabinet/admin/.top.menu.php';
 }else
 $aMenuLinks = Array(
-	Array(
-		"Проекты",
-		"/kabinet/", 
-		Array(), 
-		Array("ICON"=>"fa fa-tachometer"),
-		"" 
-	),
-	Array(
-		"Планирование",
-		"/kabinet/projects/", 
-		Array(), 
-		Array("ICON"=>"fa fa-calendar","NOLINK"=>"Y"),
-		"" 
-	),
-    Array(
-        "Финансы",
-        "/kabinet/finance/",
-        Array(),
-        Array("ICON"=>"fa fa-credit-card-alt"),
-        ""
-    ),
 );
 ?>
