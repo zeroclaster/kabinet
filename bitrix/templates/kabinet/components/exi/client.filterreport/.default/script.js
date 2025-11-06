25fb174314d300be47bae754764893f03fba2eaf
sergey <sexiterra@mail.ru>
2025-11-06 10:43:49 +0300

    Фильтр у пользователей в исполнениях схлопывается и раскрывается

--- bitrix/templates/kabinet/components/exi/client.filterreport/.default/script.js
+++ bitrix/templates/kabinet/components/exi/client.filterreport/.default/script.js
@@ -3,12 +3,77 @@ client_filter_report = (function () {
     return {
         start(PHPPARAMS) {
             $(function(){
+                const $filterContainer = $("#filter-form-container");
+                const $filterToggleBtn = $("#filter-toggle-btn");
+                const $filterToggleText = $filterToggleBtn.find(".filter-toggle-text");
+                const $filterToggleIcon = $filterToggleBtn.find(".filter-toggle-icon i");
 
                 const $fromDatepicker = $("#fromdate1");
                 const $toDatepicker = $("#todate1");
                 const findform = BX.findChild(document.body,{attribute:{name:'clientfindreportform'}},true,false);
-				
-				BX.bind(findform.alertfind,'click',(e)=>findform.submit());
+
+                // Находим чекбокс "Требует вашего внимания" вне формы
+                const alertCheckbox = document.getElementById('alertfind');
+
+                // Функция для получения куки
+                function getCookie(name) {
+                    const matches = document.cookie.match(new RegExp(
+                        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+                    ));
+                    return matches ? decodeURIComponent(matches[1]) : undefined;
+                }
+
+                // Функция для установки куки
+                function setCookie(name, value, days = 365) {
+                    const date = new Date();
+                    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+                    const expires = "expires=" + date.toUTCString();
+                    document.cookie = name + "=" + encodeURIComponent(value) + ";" + expires + ";path=/";
+                }
+
+                // Уникальное имя куки для этого фильтра
+                const cookieName = 'filter_state_' + (PHPPARAMS.COMPONENT_NAME || 'default');
+
+                // Проверяем состояние фильтра из куки
+                const filterState = getCookie(cookieName);
+                const isFilterOpen = filterState === 'open';
+
+                // Устанавливаем начальное состояние
+                if (isFilterOpen) {
+                    $filterContainer.show();
+                    $filterToggleText.text('Скрыть фильтр');
+                    $filterToggleIcon.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+                } else {
+                    $filterContainer.hide();
+                    $filterToggleText.text('Фильтр');
+                    $filterToggleIcon.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+                }
+
+                // Обработчик переключения фильтра
+                BX.bind($filterToggleBtn[0], 'click', function(e) {
+                    if ($filterContainer.is(":visible")) {
+                        $filterContainer.slideUp(300);
+                        $filterToggleText.text('Фильтр');
+                        $filterToggleIcon.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+                        setCookie(cookieName, 'closed');
+                    } else {
+                        $filterContainer.slideDown(300);
+                        $filterToggleText.text('Скрыть фильтр');
+                        $filterToggleIcon.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+                        setCookie(cookieName, 'open');
+                    }
+                });
+
+                // Обработчик для чекбокса "Требует вашего внимания"
+                BX.bind(alertCheckbox, 'click', (e) => {
+                    // Создаем скрытое поле в форме и отправляем
+                    const hiddenInput = document.createElement('input');
+                    hiddenInput.type = 'hidden';
+                    hiddenInput.name = 'alert';
+                    hiddenInput.value = alertCheckbox.checked ? 'y' : '';
+                    findform.appendChild(hiddenInput);
+                    findform.submit();
+                });
 
                 // Добавляем обработчик нажатия Enter для поля "ID исполнения"
                 const queueInput = findform.querySelector('input[name="queue"]');
@@ -35,8 +100,6 @@ client_filter_report = (function () {
                 const datepicker2 = $fromDatepicker.data('DateTimePicker');
 
                 $fromDatepicker.on('dp.change', (event) => {
-                    //console.log(event.date);
-
                     if (event.date) {
                         const newDate = moment(event.date, "DD.MM.YYYY");
                         let d = $toDatepicker.val();
@@ -47,11 +110,9 @@ client_filter_report = (function () {
                         datepicker1.minDate(false);
                         if (!d) datepicker1.date(null);
                     }
-
                 });
 
                 $toDatepicker.on('dp.change', (event) => {
-                    //console.log(event.date);
                     if (event.date) {
                         const newDate = moment(event.date, "DD.MM.YYYY");
                         let d = $fromDatepicker.val();
@@ -62,21 +123,18 @@ client_filter_report = (function () {
                         datepicker2.maxDate(false);
                         if (!d) datepicker2.date(null);
                     }
-
                 });
 
                 BX.bind(BX('clearform'),'click',function (e) {
-                    // TODO AKULA разобраться почему не работает form.reset()
-                    //findform.reset();
-
                     for(index in findform.elements)
                         findform.elements[index].value = '';
 
+                    // Также сбрасываем внешний чекбокс
+                    alertCheckbox.checked = false;
+
                     findform.clearflag.value = 'y';
-
                     findform.submit();
-                })
-
+                });
             });
         }
     }
--- bitrix/templates/kabinet/components/exi/client.filterreport/.default/template.php
+++ bitrix/templates/kabinet/components/exi/client.filterreport/.default/template.php
@@ -14,7 +14,6 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 
-
 Loc::loadMessages(__FILE__);
 $this->setFrameMode(true);
 
@@ -25,60 +24,79 @@ $SEARCH_RESULT = $arResult['SEARCH_RESULT'];
 ?>
 
 <div class="mt-1 clint-filter-report">
-<form action="" name="clientfindreportform" enctype="multipart/form-data" method="post">
-<div class="row">
+    <!-- Блок "Требует вашего внимания" вынесен перед фильтром -->
+    <div class="row mb-2">
+        <div class="col-md-12 d-flex  justify-content-start align-items-center">
+            <!-- Кнопка переключения фильтра -->
+            <button type="button" class="btn btn-outline-secondary btn-sm mr-3" id="filter-toggle-btn">
+                <span class="filter-toggle-text">Фильтр</span>
+                <span class="filter-toggle-icon"><i class="fa fa-chevron-down"></i></span>
+            </button>
 
-    <div class="col-md-4">
-        <div class="d-flex no-d-flex">
-            <div class="d-flex align-items-center"><button type="button" class="btn btn-link btn-sm">За период</button></div>
-
-            <div class="mr-2 d-flex align-items-center">с</div>
-            <div class="mr-2"><input name="fromdate1" id="fromdate1" value="<?=$SEARCH_RESULT['fromdate1']?>" type="text" class="form-control" style="width: 123px;"></div>
-            <div class="mr-2 d-flex align-items-center">по</div>
-            <div><input name="todate1" id="todate1" value="<?=$SEARCH_RESULT['todate1']?>" type="text" class="form-control" style="width: 123px;"></div>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div class="d-flex no-d-flex">
-            <div class="d-flex align-items-center mr-3" style="width: 100px;">Со статусом:</div>
-            <div class="d-flex align-items-center">
-                <select name="statusfind" id="statusfind" class="form-control">
-                    <option value=""></option>
-                    <?foreach ($runnerManager->getStatusList() as $status_id => $title):?>
-                        <option value="<?=$status_id?>" <?if (is_numeric($SEARCH_RESULT['statusfind']) && $SEARCH_RESULT['statusfind']==$status_id) echo "selected"?>><?=$title?></option>
-                    <?endforeach;?>
-                </select>
+            <!-- Блок "Требует вашего внимания" -->
+            <div class="alert-status-find">
+                <input id="alertfind" name="alert" type="checkbox" value="y" <?if($SEARCH_RESULT['alert']) echo 'checked';else echo '';?>>
+                <label for="alertfind" class="alert-no-checked <?if($arResult['count_alert']) echo "alert-checked";?>">
+                    Требует вашего внимания:
+                    <span class="badge badge-iphone-style"><?=$arResult['count_alert']?></span>
+                </label>
             </div>
         </div>
     </div>
-    <div class="col-md-2 align-self-center alert-status-find">
-		<input id="alertfind" name="alert" type="checkbox" value="y" <?if($SEARCH_RESULT['alert']) echo 'checked';else echo '';?>>
-        <label for="alertfind" class="alert-no-checked <?if($arResult['count_alert']) echo "alert-checked";?>">Требует вашего внимания:  <span class="badge badge-iphone-style"><?=$arResult['count_alert']?></span></label>
-    </div>
-    <div class="col-md-3">
-        <div class="d-flex">
-            <div class="d-flex align-items-center" style="width: 104px;">Исполнение #</div>
-            <div class="d-flex align-items-center">
-                <input class="form-control" type="text" name="queue" value="<?=$SEARCH_RESULT['queue']?>" style="width: 121px;">
+
+    <!-- Форма фильтра -->
+    <div id="filter-form-container" style="display: none;">
+        <form action="" name="clientfindreportform" enctype="multipart/form-data" method="post">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="d-flex no-d-flex">
+                        <div class="d-flex align-items-center"><button type="button" class="btn btn-link btn-sm">За период</button></div>
+                        <div class="mr-2 d-flex align-items-center">с</div>
+                        <div class="mr-2"><input name="fromdate1" id="fromdate1" value="<?=$SEARCH_RESULT['fromdate1']?>" type="text" class="form-control" style="width: 123px;"></div>
+                        <div class="mr-2 d-flex align-items-center">по</div>
+                        <div><input name="todate1" id="todate1" value="<?=$SEARCH_RESULT['todate1']?>" type="text" class="form-control" style="width: 123px;"></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="d-flex no-d-flex">
+                        <div class="d-flex align-items-center mr-3" style="width: 100px;">Со статусом:</div>
+                        <div class="d-flex align-items-center">
+                            <select name="statusfind" id="statusfind" class="form-control">
+                                <option value=""></option>
+                                <?foreach ($runnerManager->getStatusList() as $status_id => $title):?>
+                                    <option value="<?=$status_id?>" <?if (is_numeric($SEARCH_RESULT['statusfind']) && $SEARCH_RESULT['statusfind']==$status_id) echo "selected"?>><?=$title?></option>
+                                <?endforeach;?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="d-flex">
+                        <div class="d-flex align-items-center" style="width: 104px;">Исполнение #</div>
+                        <div class="d-flex align-items-center">
+                            <input class="form-control" type="text" name="queue" value="<?=$SEARCH_RESULT['queue']?>" style="width: 121px;">
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+
+            <div class="row justify-content-md-center mt-3">
+                <div class="col-md-8 text-center">
+                    <button type="submit" class="btn btn-primary mr-5">Показать</button>
+                    <button id="clearform" type="button" class="btn btn-link btn-sm">Все</button>
+                    <input type="hidden" name="clearflag" value="">
+                </div>
+            </div>
+        </form>
     </div>
 </div>
 
-    <div class="row justify-content-md-center">
-        <div class="col-md-8 text-center">
-            <button type="submit" class="btn btn-primary mr-5">Показать</button> <button id="clearform" type="button" class="btn btn-link btn-sm">Все</button>
-            <input type="hidden" name="clearflag" value="">
-        </div>
-    </div>
-</form>
-</div>
 <?
 $jsParams = [
-    'SEARCH_RESULT' => $arResult['SEARCH_RESULT']
+    'SEARCH_RESULT' => $arResult['SEARCH_RESULT'],
+    'COMPONENT_NAME' => $this->GetComponent()->getName() // Добавляем имя компонента для уникальности куки
 ];
 ?>
 <script>
-    // installe
     client_filter_report.start(<?=CUtil::PhpToJSObject($jsParams, false, true)?>);
 </script>
